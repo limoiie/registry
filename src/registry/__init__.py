6f4253757d4705904b46a01328e35c9e65dc2fab
limoiie <limo.iie4@gmail.com>
2022-11-23 12:59:46 +0800

    refactor: Hide importlib from the export interface

--- src/registry/__init__.py
+++ src/registry/__init__.py
@@ -1,14 +1,14 @@
 __all__ = ['__version__', 'Registry', 'SubclassRegistry']
 
 try:
-    from importlib.metadata import version, PackageNotFoundError
+    import importlib.metadata as _importlib_metadata
 except ModuleNotFoundError:
     # noinspection PyUnresolvedReferences
-    from importlib_metadata import version, PackageNotFoundError
+    import importlib_metadata as _importlib_metadata
 
 try:
-    __version__ = version("registry")
-except PackageNotFoundError:
+    __version__ = _importlib_metadata.version("registry")
+except _importlib_metadata.PackageNotFoundError:
     __version__ = "unknown version"
 
 from .registry import Registry
